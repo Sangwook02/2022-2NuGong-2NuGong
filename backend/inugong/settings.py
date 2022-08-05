a578b7a1d11daaa1de8b3eaca00a6ce25bdd4223
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2022-08-06 00:18:48 +0900

    Update settings.py

--- backend/inugong/settings.py
+++ backend/inugong/settings.py
@@ -176,3 +176,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
 
 }
+
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
